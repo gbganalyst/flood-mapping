0b5b4b5bc07c2c13462710a79283ce58b233907e
gbganalyst <gbganalyst@gmail.com>
2020-02-26 17:10:33 +0100

    Update summary page

--- functions/index.js
+++ functions/index.js
@@ -27,6 +27,10 @@ app.get('/oyo-farms', (req, res) => {
     res.render('oyo-farms')
 })
 
+app.get('/team', (req, res) => {
+    res.render('team')
+})
+
 app.get('/flood-area', (req, res) => {
     res.render('flood-area')
 })
